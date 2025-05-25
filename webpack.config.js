a345244fb8167a291707b003bcc754f49f9f1df8
ParasTemkar <parastemkar0204@gmail.com>
2025-05-25 11:32:29 +0530

    Updated to implement hot Module Replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "development",
@@ -8,15 +9,30 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: "/",
   },
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/index.html"],
+    // If you have a separate 'public' folder for static assets (like images, fonts)
+    // that are NOT processed by Webpack, uncomment and adjust this:
+    // static: {
+    //   directory: path.resolve(__dirname, 'public'),
+    //   publicPath: '/',
+    // },
+    watchFiles: [
+      "./src/**/*",
+      // If you uncommented 'static' above and had a 'public' folder, you might add: "./public/**/*"
+    ],
+    hot: true,
+    open: true,
+    port: 8080,
+    // historyApiFallback: true, // Useful for single-page applications (SPAs) with client-side routing
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
     rules: [
@@ -28,10 +44,10 @@ module.exports = {
       {
         test: /\.html$/i,
         loader: "html-loader",
-      }, 
+      },
       */
 
-		/* if there are images in JS files.
+      /* If there are images in JS files or you want to handle other asset types.
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
