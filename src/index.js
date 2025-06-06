e7cf201928d92cb06639191b5c90203208b53228
ParasTemkar <parastemkar0204@gmail.com>
2025-06-06 19:36:51 +0530

    Added Modal with form as input method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { ToDo } from "./todoCreate";    
+import './styles.css';
+import { ToDo } from './todoCreate.js';
 import './todoModal.js';
 
-const todo1 = new ToDo('IDBVersi','uhbgih','hbfi','2022-03-03','ifi');
-console.log(todo1)
 
-console.log("Online")
+
+
 
--- src/todoModal.js
+++ src/todoModal.js
@@ -1,4 +1,23 @@
-const modalWindow = document.querySelector('#modalWindow');
+import { ToDo } from "./todoCreate"; 
 
-document.querySelector('#modalOpen').addEventListener('click', ()=> modalWindow.style.display = "block");
-document.querySelector('#modalClose').addEventListener('click', ()=> modalWindow.style.display = "none");
+const modalWindow = document.querySelector('#modalWindow');
+const myLibrary = [];
+
+document.querySelector('#modalOpen').addEventListener('click', () => modalWindow.style.display = "block");
+document.querySelector('#modalClose').addEventListener('click', () => modalWindow.style.display = "none");
+
+document.getElementById("todo-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("input[name='title']").value;
+  const note = document.querySelector("textarea[name='note']").value;
+  const duedate = document.querySelector("input[name='date']").value;
+  const priority = document.querySelector("select[name='priority']").value;
+
+  const todo = new ToDo(title, note, duedate, priority);
+  myLibrary.push(todo);
+
+  console.log(todo);
+
+  modalWindow.style.display = "none";
+});
